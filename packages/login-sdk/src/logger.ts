48df16267c68499724c049b4f747d5af5570a7e4
Fran Zekan <zekan.fran369@gmail.com>
2025-06-23 12:07:33 +0200

    Color to logger and allow it to be used outside of auth

--- packages/login-sdk/src/logger.ts
+++ packages/login-sdk/src/logger.ts
@@ -1,19 +1,21 @@
 export class Logger {
   enabled: boolean;
+  prefix: string;
 
-  constructor(enabled = true) {
+  constructor(enabled = true, prefix = "[Unidy Auth]") {
     this.enabled = enabled;
+    this.prefix = prefix;
   }
 
   log(...args: unknown[]) {
     if (this.enabled) {
-      console.log("[Unidy Auth]", ...args);
+      console.log(`%c${this.prefix}`, "color: blue; font-weight: bold", ...args);
     }
   }
 
   error(...args: unknown[]) {
     if (this.enabled) {
-      console.error("[Unidy Auth]", ...args);
+      console.error(`%c${this.prefix}`, "color: red; font-weight: bold", ...args);
     }
   }
 }
