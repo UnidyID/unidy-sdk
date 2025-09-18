c64c94becd5f8c35df44e1c06db1f3dcf9de143a
MandyGottschling <mandy.gottschling@unidy.de>
2025-09-18 17:30:07 +0200

    WIP Read only multi Select field

--- packages/unidy-profile/src/components/unidy-field/unidy-field.tsx
+++ packages/unidy-profile/src/components/unidy-field/unidy-field.tsx
@@ -90,6 +90,17 @@ export class UnidyField {
     };
   };
 
+  private multiSelectLabel = (fieldData: any): string[] => {
+    let multiselectMatches: string[] = [];
+    Array.isArray(fieldData.value)
+              ? fieldData.value.map((val: string) => {
+                  const match = fieldData.options?.find((opt: any) => opt.value === val);
+                  multiselectMatches.push(match?.label ?? val);
+                })
+              : [];
+    return multiselectMatches;
+  };
+
   render() {
     if (this.store.state.loading) {
       return <div class="spinner"/>;
@@ -102,15 +113,24 @@ export class UnidyField {
     const isLocked = !!fieldData?.locked;
     const lockedText = fieldData?.locked_text ? fieldData.locked_text : "";
     const isReadonly = fieldData?.readonly === true;
+    const multiSelectReadonlyLabels = this.multiSelectLabel(fieldData);
     // TODO: Add other types
-
     return (
       <div>
         <label htmlFor={this.field} part="label">
           {fieldData?.label}
           {fieldData?.required || this.required ? <span part="required-indicator"> *</span> : null}
         </label>
-        {isReadonly ? <span part="readonly-indicator">{fieldData?.value || this.readonlyPlaceholder}</span> : null}
+        {isReadonly && fieldData?.type !== 'checkbox' ? <span part="readonly-indicator">{fieldData?.value || this.readonlyPlaceholder}</span> : null}
+        {isReadonly && fieldData?.type === "checkbox" && (
+          <div part="multi-select-readonly-container">
+            {multiSelectReadonlyLabels.map((label, index) => (
+              <span key={index} part="multi-select-readonly-field">
+                {label}
+              </span>
+            ))}
+          </div>
+        )}
         {!isReadonly && (
           fieldData.type === "select" && fieldData.options ? (
             <select
