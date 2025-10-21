bbe2fb93ed167150cc09e6f6a3cdcbe6d3102da5
MandyGottschling <mandy.gottschling@unidy.de>
2025-10-21 15:58:41 +0200

    Fix: linter

--- packages/auth/src/components/auth/logout-button/logout-button.tsx
+++ packages/auth/src/components/auth/logout-button/logout-button.tsx
@@ -6,7 +6,7 @@ import { Auth } from "../../../auth";
   shadow: false,
 })
 export class LogoutButton {
-  @Prop() text: string = "Logout";
+  @Prop() text = "Logout";
   @Prop() customStyle = "";
 
   private async handleLogout() {
