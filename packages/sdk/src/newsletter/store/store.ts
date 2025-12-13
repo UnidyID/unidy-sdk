dae16fc238ed7549fcca8ea05b42debbba8cbcce
Kresimir Topolovec <k.topolovec95@gmail.com>
2025-12-13 15:31:56 +0100

    Fix index.html

--- packages/sdk/src/newsletter/store/store.ts
+++ packages/sdk/src/newsletter/store/store.ts
@@ -5,15 +5,30 @@ const store = createStore({
   checkedNewsletters: [],
 });
 
+const newsletterStoreOnChange: <K extends keyof typeof store.state>(prop: K, cb: (value: typeof store.state[K]) => void) => () => void = store.onChange;
+
 class NewsletterStore {
   get state() {
     return store.state;
   }
 
+  get<K extends keyof typeof store.state>(key: K): typeof store.state[K] {
+    return store.state[key];
+  }
+
+  set<K extends keyof typeof store.state>(key: K, value: typeof store.state[K]): void {
+    store.state[key] = value;
+  }
+
   setEmail(email: string) {
     store.state.email = email;
+  }
+
+  onChange<K extends keyof typeof store.state>(prop: K, cb: (value: typeof store.state[K]) => void): () => void {
+    return newsletterStoreOnChange(prop, cb);
   }
 }
 
 export const newsletterStore = new NewsletterStore();
 export default newsletterStore;
+export { newsletterStoreOnChange as onChange };
