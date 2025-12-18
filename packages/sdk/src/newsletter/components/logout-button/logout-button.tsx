885fa0ac7920f5ffc5676009e4c8a846fd7de989
Kresimir Topolovec <k.topolovec95@gmail.com>
2025-12-19 00:16:49 +0100

    Update logout button

--- packages/sdk/src/newsletter/components/logout-button/logout-button.tsx
+++ packages/sdk/src/newsletter/components/logout-button/logout-button.tsx
@@ -1,25 +1,44 @@
-import { Component, h, Prop } from "@stencil/core";
+import { Component, h, Prop, Element, Host } from "@stencil/core";
 import { newsletterLogout } from "../../newsletter-helpers";
 import { t } from "../../../i18n";
 import { Flash } from "../../../shared/store/flash-store";
+import { newsletterStore } from "../../store/newsletter-store";
+import { hasSlotContent } from "../../../shared/component-utils";
 
 @Component({
   tag: "u-newsletter-logout-button",
   shadow: false,
 })
 export class LogoutButton {
+  @Element() el!: HTMLElement;
+
   @Prop({ attribute: "class-name" }) componentClassName = "";
 
   private handleLogout = () => {
     newsletterLogout();
-    Flash.success.addMessage(t("newsletter.logout_success"));
+    Flash.success.addMessage(t("newsletter.success.logout"));
   };
 
   render() {
+    if (newsletterStore.state.preferenceToken === "" || newsletterStore.state.isAuthenticated) {
+      return <Host style={{ display: "none" }} />;
+    }
+
+    const hasContent = hasSlotContent(this.el);
+
     return (
-      <button type="button" class={this.componentClassName} onClick={this.handleLogout} aria-live="polite">
-        {t("buttons.logout")}
-      </button>
+      <Host>
+        <button
+          type="button"
+          part="button"
+          class={this.componentClassName}
+          onClick={this.handleLogout}
+          aria-label={hasContent ? "Logout" : undefined}
+          aria-live="polite"
+        >
+          {hasContent ? <slot /> : t("newsletter.buttons.logout")}
+        </button>
+      </Host>
     );
   }
 }
