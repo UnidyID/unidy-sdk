bd9d813f9b7903a7b91883508ab8d6a0dc5947d3
Fran Zekan <zekan.fran369@gmail.com>
2026-01-06 19:52:49 +0100

    update generated stuff

--- packages/sdk/src/components.d.ts
+++ packages/sdk/src/components.d.ts
@@ -176,6 +176,14 @@ export namespace Components {
         "componentClassName"?: string;
         "internalName": string;
         "preferenceIdentifier": string;
+        /**
+          * Public method to set the checkbox state programmatically
+         */
+        "setChecked": (checked: boolean) => Promise<void>;
+        /**
+          * Public method to toggle the checkbox programmatically
+         */
+        "toggle": () => Promise<void>;
     }
     interface UNewsletterResendDoiButton {
         /**
