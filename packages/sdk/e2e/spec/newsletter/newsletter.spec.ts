8f66cc32ae4d1089c5f2d8869e99110d229e7f10
MandyGottschling <mandy.gottschling@unidy.de>
2026-01-19 13:26:07 +0100

    Add more newsletter specs

--- packages/sdk/e2e/spec/newsletter/newsletter.spec.ts
+++ packages/sdk/e2e/spec/newsletter/newsletter.spec.ts
@@ -1,5 +1,6 @@
 import { expect, test } from "@playwright/test";
-import { routes } from "../../config";
+import { use } from "react";
+import { routes, userLogin } from "../../config";
 
 test.describe("Newsletter", () => {
   test.beforeEach(async ({ page }) => {
@@ -17,5 +18,41 @@ test.describe("Newsletter", () => {
     await expect(page.getByTestId("nl.group.main.pref.player_news")).toBeVisible();
     await expect(page.getByTestId("nl.group.main.pref.club_news")).toBeVisible();
     await expect(page.getByTestId("nl.group.main.pref.random_news")).toBeVisible();
+    await expect(page.getByTestId("nl.group.main.switch")).toHaveAttribute("checked", "true");
+    await expect(page.getByTestId("nl.group.main.pref.club_news")).toHaveAttribute("checked", "true");
+  });
+
+  test("enables Subscribe button and shows 'Already subscribed' action after entering email", async ({ page }) => {
+    const emailInput = page.getByRole("textbox", { name: "Email" });
+    const subscribeButton = page.getByRole("button", { name: "Subscribe", exact: true });
+
+    await emailInput.fill(userLogin.email);
+    await expect(subscribeButton).toBeEnabled();
+    await expect(page.getByRole("button", { name: "Already subscribed? Click" })).toBeVisible();
+  });
+
+  test("subscribes successfully and disables preference controls", async ({ page }) => {
+    const emailInput = page.getByRole("textbox", { name: "Email" });
+    const subscribeButton = page.getByRole("button", { name: "Subscribe", exact: true });
+
+    await emailInput.fill(userLogin.email);
+    await subscribeButton.click();
+
+    await expect(page.getByText("You have successfully subscribed")).toBeVisible();
+    await expect(page.getByTestId("nl.group.main.switch")).toBeDisabled();
+    await expect(page.getByTestId("nl.group.main.pref.player_news")).toBeDisabled();
+    await expect(page.getByTestId("nl.group.main.pref.club_news")).toBeDisabled();
+    await expect(page.getByTestId("nl.group.main.pref.random_news")).toBeDisabled();
+  });
+
+  test("shows error message when already subscribed", async ({ page }) => {
+    const emailInput = page.getByRole("textbox", { name: "Email" });
+    const subscribeButton = page.getByRole("button", { name: "Subscribe", exact: true });
+
+    await emailInput.fill(userLogin.email);
+    await subscribeButton.click();
+
+    await expect(page.getByTestId("nl.group.main").locator("u-error-message")).toBeVisible();
+    await expect(page.getByText("We sent a link to manage your")).toBeVisible();
   });
 });
