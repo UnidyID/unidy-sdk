419a539369fcd9d70ce5db5a6490ae49ae3d40c0
MandyGottschling <mandy.gottschling@unidy.de>
2026-01-27 16:07:39 +0100

    Add header param to testApi

--- packages/sdk/e2e/lib/testApi.ts
+++ packages/sdk/e2e/lib/testApi.ts
@@ -1,5 +1,6 @@
 // copied from UnidyCode
 const apiURL = process.env.E2E_API_URL || "http://127.0.0.1:3000";
+const apiToken = process.env.E2E_API_TOKEN || "";
 
 export class TestApi {
   async fetch<T = object>(url: string, options: Omit<RequestInit, "body"> & { body?: any } = {}, throwOnError = true) {
@@ -9,6 +10,7 @@ export class TestApi {
       headers: {
         ...(options.body && typeof options.body === "object" && { "Content-Type": "application/json" }),
         ...options.headers,
+        "X-Test-Secret": apiToken,
       },
     });
 
