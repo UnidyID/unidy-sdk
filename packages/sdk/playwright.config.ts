62a1ef030a80b10047d91299624a184b4c5fe0d0
Burkhard Vogel-Kreykenbohm <burkhard.vogel@unidy.de>
2026-01-27 11:48:08 +0100

    feat: running playwright in CI

--- packages/sdk/playwright.config.ts
+++ packages/sdk/playwright.config.ts
@@ -1,13 +1,7 @@
 import "dotenv/config";
 import { defineConfig, devices } from "@playwright/test";
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// import dotenv from 'dotenv';
-// import path from 'path';
-// dotenv.config({ path: path.resolve(__dirname, '.env') });
+const baseURL = process.env.E2E_BASE_URL || "http://localhost:3333";
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -27,7 +21,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: "http://localhost:3333",
+    baseURL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -85,7 +79,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "bun run dev",
-    url: "http://localhost:3333",
+    url: baseURL,
     reuseExistingServer: !process.env.CI,
     timeout: 120_000,
   },
